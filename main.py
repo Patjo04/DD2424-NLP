d2da1b41d656de383fe8caabb73f2b5a1720bdbd
Patrik Johansson <patrikj8@kth.se>
2024-04-22 15:05:29 +0200

    Inkluderade en random-modul

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
 #Startprogram: Skriv ut ditt namn och få ett hej tillbaka!
 def main(namn):
     print("Hej hopp " + str(namn) + "!")
+    print("Ditt lyckotal för nu är: " + str(random.randint(0, 2147483647)))
 
 main(input())

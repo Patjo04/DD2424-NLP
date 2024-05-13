4b835ef03d079146852ec2f221c5817ffc648338
Patrik Johansson <patrikj8@kth.se>
2024-05-13 10:40:33 +0200

    Börjat läsa in data

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import random
+import os
 #Startprogram: Skriv ut ditt namn och få ett hej tillbaka!
+
+
+
 def main(namn):
     print("Hej hopp " + str(namn) + "!")
     print("Ditt lyckotal för nu är: " + str(random.randint(0, 2147483647)))
+    
+    files = 0
+
+    while files < 50:
+        file = os.getpath(()"", fn in os.getpath(""))
+        open(file) as f:
+            for line in f.getLines():
+            cleanline(line)
+
+
+# LSTM som bas
+# tanh-layer är nog ok
+
+# 2 delar: Data och network
+# data tar in en eller flera C-filer och då producerar flera datapunkter för träning och testande
+
+# Neural modulen är som language 4. Där skapas en lstm.
+# Färdig E så fort som möjligt, GRU mer avancerat än en lstm.
 
 main(input())
